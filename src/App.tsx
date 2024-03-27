ddf7d19e9701e121453d462eaf88f4c22399ec96
HamidGX <hamid.uxw@gmail.com>
2024-03-27 01:14:45 -0600

    feat add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Home from '@/pages/Home'
+import Layout from '@/components/Layout'
+
 export default function App() {
-	return <div>App</div>
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Layout />}>
+					<Route index element={<Home />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	)
 }
